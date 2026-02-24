ca01f4d01c4dfc71e498296e484b6d466a741902
Spark <79936503+spa5k@users.noreply.github.com>
2026-02-24 19:50:45 +0530

    Update happymode cask to v0.0.1

--- Casks/happymode.rb
+++ Casks/happymode.rb
@@ -1,8 +1,8 @@
 cask "happymode" do
-  version :latest
-  sha256 :no_check
+  version "0.0.1"
+  sha256 "8252e693bfda6f1533cc33cf89c251598da03bd48b8b169a420656612f3c049e"
 
-  url "https://github.com/happytoolin/happymode/releases/latest/download/happymode-latest.zip"
+  url "https://github.com/happytoolin/happymode/releases/download/v#{version}/happymode-v#{version}.zip"
   name "happymode"
   desc "Menu bar app that switches Light/Dark mode based on sunrise and sunset"
   homepage "https://github.com/happytoolin/happymode"
